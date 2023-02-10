c74e5866108c1c159975cc80dd798959cceca417
achchaimae <achchaimae1@gmail.com>
2023-02-10 20:54:51 +0100

    db

--- backend/monsalonline.sql
+++ backend/monsalonline.sql
@@ -1,4 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 10, 2023 at 08:24 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `monsalonline`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointement`
+--
 
 CREATE TABLE `appointement` (
   `id` int(11) NOT NULL,
@@ -6,11 +33,18 @@ CREATE TABLE `appointement` (
   `client_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
+--
+-- Dumping data for table `appointement`
+--
 
 INSERT INTO `appointement` (`id`, `date`, `client_id`) VALUES
-(6, '2023-02-09 10:00:00', 1);
+(7, '2023-02-09 12:00:00', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
 
 CREATE TABLE `client` (
   `id` int(11) NOT NULL,
@@ -20,12 +54,22 @@ CREATE TABLE `client` (
   `ref` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
+--
+-- Dumping data for table `client`
+--
 
 INSERT INTO `client` (`id`, `first_name`, `last_name`, `phone`, `ref`) VALUES
-(1, 'Oussama', 'Haddi', '0606060606', 'GkjnzfgtZRF');
+(1, 'Oussama', 'Haddi', '0606060606', 'GkjnzfgtZRF'),
+(3, 'rabie', 'ouallaf', '093847293774', 'GHZKRIJJRHHFYZ'),
+(121, 'oussama', 'Ouafidi', '0684842384', 'X-014d232384');
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `appointement`
+--
 ALTER TABLE `appointement`
   ADD PRIMARY KEY (`id`),
   ADD KEY `client_id` (`client_id`);
@@ -36,18 +80,33 @@ ALTER TABLE `appointement`
 ALTER TABLE `client`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `appointement`
+--
 ALTER TABLE `appointement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
 
+--
+-- Constraints for dumped tables
+--
 
-
+--
+-- Constraints for table `appointement`
+--
 ALTER TABLE `appointement`
   ADD CONSTRAINT `appointement_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
